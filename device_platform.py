844695e3b67a1814b27839509cd76e12ff27c1b4
synackuk <53281989+synackuk@users.noreply.github.com>
2020-07-09 19:13:19 +0100

    A5

--- device_platform.py
+++ device_platform.py
@@ -14,7 +14,7 @@ class DevicePlatform:
     self.nonce_length     = nonce_length
     self.sep_nonce_length = sep_nonce_length
     self.demotion_reg     = demotion_reg
-    if self.cpid in [0x8940, 0x8947]:
+    if self.cpid in [0x8940, 0x8942, 0x8945, 0x8947]:
       self.dfu_image_base      = 0x34000000
       self.dfu_load_base       = 0x9FF00000
       self.recovery_image_base = 0x9FF00000
@@ -54,6 +54,27 @@ class DevicePlatform:
       return 't%xsi' % self.cpid
 
 all_platforms = [
+  DevicePlatform(cpid=0x8940, cprv=0x21, scep=0x01, arch='armv7', srtg='iBoot-838.3',
+    rom_base=0x3F000000, rom_size=0x10000, rom_sha1='d9320ddd4bdb1de79ae0601f20e7db23441ab1a7',
+    sram_base=0x34000000, sram_size=0x40000,
+    dram_base=0x80000000,
+    nonce_length=20, sep_nonce_length=None,
+    demotion_reg=0x3F500000,
+  ),
+  DevicePlatform(cpid=0x8942, cprv=0x10, scep=0x10, arch='armv7', srtg='rom',
+    rom_base=0x3F000000, rom_size=0x10000, rom_sha1='d9320ddd4bdb1de79ae0601f20e7db23441ab1a7',
+    sram_base=0x34000000, sram_size=0x40000,
+    dram_base=0x80000000,
+    nonce_length=20, sep_nonce_length=None,
+    demotion_reg=0x3F500000,
+  ),
+  DevicePlatform(cpid=0x8945, cprv=0x21, scep=0x01, arch='armv7', srtg='iBoot-1062.2',
+    rom_base=0x3F000000, rom_size=0x10000, rom_sha1='d9320ddd4bdb1de79ae0601f20e7db23441ab1a7',
+    sram_base=0x34000000, sram_size=0x40000,
+    dram_base=0x80000000,
+    nonce_length=20, sep_nonce_length=None,
+    demotion_reg=0x3F500000,
+  ),
   DevicePlatform(cpid=0x8947, cprv=0x00, scep=0x10, arch='armv7', srtg='iBoot-1458.2',
     rom_base=0x3F000000, rom_size=0x10000, rom_sha1='d9320ddd4bdb1de79ae0601f20e7db23441ab1a7',
     sram_base=0x34000000, sram_size=0x40000,
